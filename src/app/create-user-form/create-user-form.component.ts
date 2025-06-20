c47f799d60d606d0a66c97fb6c80029dcd13e398
Hadjimurad <hadjimurad997@mail.ru>
2025-06-21 01:06:03 +0400

    New interface for form

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -2,13 +2,16 @@ import { Component, EventEmitter } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Output } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component ({
   selector: 'app-create-user-form',
   templateUrl: './create-user-form.component.html',
   styleUrl: './create-user-form.component.scss',
   standalone: true,
-  imports: [ ReactiveFormsModule, NgIf ]
+  imports: [ ReactiveFormsModule, NgIf, MatButtonModule, MatFormFieldModule, MatInputModule ]
 })
 
 export class createUserFormComponent {
@@ -16,14 +19,14 @@ export class createUserFormComponent {
  createUser = new EventEmitter();
 
   public form = new FormGroup ({
-    name: new FormControl('hello world', [Validators.required, Validators.minLength(2)]),
-    email: new FormControl('тестовый эмейл', [Validators.required, Validators.email]),
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
     website: new FormControl('', [Validators.required, Validators.minLength(3)]),
     company: new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(2)])
     })
   })
-  
+
   public submitForm(): void {
     this.createUser.emit(this.form.value)
     this.form.reset();
